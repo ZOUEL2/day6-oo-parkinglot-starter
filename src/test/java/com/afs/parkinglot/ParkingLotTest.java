0c011e8b4c0e0f518e155fbeb450081942897fac
ZOUEL2 <zouel2@oocl.com>
2025-09-08 20:29:53 +0800

    feat:park multiple cars and fetch them correctly

--- src/test/java/com/afs/parkinglot/ParkingLotTest.java
+++ src/test/java/com/afs/parkinglot/ParkingLotTest.java
@@ -3,8 +3,7 @@ package com.afs.parkinglot;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ParkingLotTest {
     private ParkingLot parkingLot;
@@ -22,6 +21,19 @@ public class ParkingLotTest {
         assertNotNull(plateTicket);
         assertEquals(testCar, parkingLot.fetch(plateTicket));
     }
+
+    @Test
+    void should_park_multiple_cars_and_fetch_them_correctly() {
+        Car car1 = new Car();
+        Car car2 = new Car();
+        PlateTicket ticket1 = parkingLot.park(car1);
+        PlateTicket ticket2 = parkingLot.park(car2);
+        assertNotEquals(ticket1, ticket2);
+        assertEquals(car1, parkingLot.fetch(ticket1));
+        assertEquals(car2, parkingLot.fetch(ticket2));
+    }
+
+
 
 
 
